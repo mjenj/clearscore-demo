acd00e8c9280737a8cbc2a38baa5568b69db0b76
Matt <matt@skynamo.com>
2023-01-27 16:17:02 +0200

    initial commit

---
+++ clearscore-demo/clearscore_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  clearscore_demoApp.swift
+//  clearscore-demo
+//
+//  Created by Matt Jenje on 2023/01/27.
+//
+
+import SwiftUI
+
+@main
+struct clearscore_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clearscore-demoUITests/clearscore_demoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  clearscore_demoUITestsLaunchTests.swift
+//  clearscore-demoUITests
+//
+//  Created by Matt Jenje on 2023/01/27.
+//
+
+import XCTest
+
+final class clearscore_demoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
